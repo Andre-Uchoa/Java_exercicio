c6e49f677ec91356a61a0cb6e240ca3888438539
Andre-Uchoa <andreuchoa2@gmail.com>
2022-11-28 22:58:55 -0300

    aula02
     Changes not staged for commit:

---
+++ curso_programacao/src/exercicio2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class exercicio2 {
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		int cod1, cod2, qtd1, qtd2;
+		
+		double valor1, valor2, total1, total2, totalf;
+		
+		cod1 = sc.nextInt();
+		qtd1 = sc.nextInt();
+		valor1 = sc.nextDouble();
+		
+		cod2 = sc.nextInt();
+		qtd2 = sc.nextInt();
+		valor2 = sc.nextDouble();
+				
+		total1 = qtd1 * valor1 ;
+		total2 = qtd2 * valor2 ;
+		
+		totalf = total1 + total2;
+		
+		System.out.printf("O valor a pagar: R$ =%.2f%n", totalf);
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+}
